eafb79ee23a1131449a359b404f0b81526a5072c
Marek Rychly <marek.rychly@gmail.com>
2020-05-28 14:26:24 +0200

    Fixed bluez-alsa module.

--- modules/bluez-alsa.nix
+++ modules/bluez-alsa.nix
@@ -7,7 +7,7 @@ let
   cfg = config.rychly.bluealsa;
 
   # FIXME: non-root user would not have access to DBus org.bluez
-  # so we would need to add https://aur.archlinux.org/cgit/aur.git/tree/bluealsa.install?h=bluez-alsa-git
+  # so we would need to add https://aur.archlinux.org/cgit/aur.git/tree/bluealsa.install?h=bluez-alsa
   # into /etc/dbus-1 which is already managed by config.services.dbus.packages and we have no package to do this
   user = "root";
   group = "audio";
@@ -40,18 +40,30 @@ in {
       pkgs.bluez-alsa-tools	# from rychly/nixpkgs-public
     ];
 
-    # adapted from https://aur.archlinux.org/cgit/aur.git/tree/bluealsa.service?h=bluez-alsa-git
+    services.dbus.packages = [ cfg.package ];
+
+    # adapted from https://github.com/Arkq/bluez-alsa/wiki/Systemd-integration#service-unit-file
     systemd.services.bluealsa = {
-      description = "BluezAlsa proxy";
-      wantedBy = [ "multi-user.target" ];
-      requires = [ "bluetooth.service" ];
-      after = [ "bluetooth.service" ];
-      preStart = "rm /run/bluealsa/* 2>/dev/null || true";
+      description = "Bluealsa daemon";
+      documentation = [ "https://github.com/Arkq/bluez-alsa/" ];
+      after = [ "dbus-org.bluez.service" ];
+      requires = [ "dbus-org.bluez.service" ];
       serviceConfig = {
+        Type = "dbus";
+        BusName = "org.bluealsa";
+        ExecStart = "${cfg.package}/bin/bluealsa";
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateTmp = true;
+        PrivateDevices = true;
+        RemoveIPC = true;
+        RestrictAddressFamilies = "AF_UNIX AF_BLUETOOTH";
+        # also non-privileged can user be used, see ${cfg.package}/share/dbus-1/system.d/bluealsa.conf
         User = user;
         Group = group;
-        ExecStart = "${cfg.package}/bin/bluealsa";
       };
+      wantedBy = [ "bluetooth.target" ];
+      preStart = "rm /run/bluealsa/* 2>/dev/null || true";
     };
 
     # adapted from https://aur.archlinux.org/cgit/aur.git/tree/bluealsa.tmpfiles?h=bluez-alsa-git
